0da9439d9ebfd562c1af36c876621dff582b9267
katiescruggs <katie.e.scruggs@gmail.com>
2017-11-06 20:23:38 -0700

    add localStorage-based tests to Search-test

--- test/Search.test.js
+++ test/Search.test.js
@@ -3,8 +3,15 @@ import {shallow, mount} from 'enzyme';
 import Search from '../lib/Search';
 
 global.localStorage = {
-  setItem: (key, value) => {
-    key: value
+  setItem(keyword, value) {
+    global.localStorage[keyword] = value;
+  },
+  clear() {
+    global.localStorage = {
+      setItem(keyword, value) {
+        global.localStorage[keyword] = value;
+      }
+    };
   }
 }
 
@@ -37,10 +44,30 @@ describe('Search', () => {
     expect(typeof search.instance().props.locationClick).toEqual('function');
   });
 
-  it('should have an input placeholder that starts with Enter a location.', () => {
+  it('should have an input placeholder that starts with "City, ST or Zip Code"', () => {
     const search = shallow(<Search locationClick={() => {}} />);
     const inputPlaceholder = search.find('input').props().placeholder;
     expect(inputPlaceholder).toEqual('City, ST or Zip Code');
+  });
+
+  it('should change the input placeholder to zip code if the zip code is in localStorage', () => {
+    localStorage.setItem('city', '80209');
+    
+    const search = shallow(<Search locationClick={() => {}} />);
+    const inputPlaceholder = search.find('input').props().placeholder;
+
+    expect(inputPlaceholder).toEqual('80209');
+
+  });
+
+  it('should change the input placeholder to location if city, state are in localStorage', () => {
+    localStorage.setItem('city', 'Denver');
+    localStorage.setItem('state', 'CO');
+
+    const search = shallow(<Search locationClick={() => {}} />);
+    const inputPlaceholder = search.find('input').props().placeholder;
+
+    expect(inputPlaceholder).toEqual('Denver, CO');
   });
 
   it('should have a button', () => {
@@ -53,13 +80,16 @@ describe('Search', () => {
     expect(search.find('ul').length).toEqual(1);
   });
 
-  it.skip('should change update localStorage on click', () => {
+  it('should change update localStorage on click', () => {
+    localStorage.clear();
+
     const search = shallow(<Search locationClick={() => {}} />);
     search.state().value = 'Denver, CO';
-    search.simulate('click');
-    expect(global.localStorage.city).toEqual('Denver');
-    expect(global.localStorage.state).toEqual('CO');
+    expect(localStorage.city).toEqual(undefined);
+    expect(localStorage.state).toEqual(undefined);
+
+    search.children('button').simulate('click');
+    expect(localStorage.city).toEqual('Denver');
+    expect(localStorage.state).toEqual('CO');
   });
-
-
 });
